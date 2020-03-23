83b4b253505a67545899d24600710ef845fe3481
mishasvitaschyk <61791758+mishasvitaschyk@users.noreply.github.com>
2020-03-23 22:51:34 +0200

    Update AdminTourController.php

--- app/Http/Controllers/Admin/AdminTourController.php
+++ app/Http/Controllers/Admin/AdminTourController.php
@@ -11,53 +11,53 @@ class AdminTourController extends Controller
 {
     public function index()
     {
-      $tours = App\Tour::paginate(12);
-      return view('admin.tour.admintour', compact('tours'));
+        $tours = App\Tour::paginate(12);
+        return view('admin.tour.admintour', compact('tours'));
     }
 
     public function create()
     {
-      return view('admin.tour.tour_create');
+        return view('admin.tour.tour_create');
     }
 
     public function store(TourRequest $tour)
     {
-      $photo = $tour->file('image')->store('uploads', 'public');
-      Tour::create([
-        'tour' => request('tour'),
-        'content' => request('content'),
-        'price' => request('price'),
-        'photo' => $photo,
-      ]);
-      return back()->with('success','Дані були добавлені');
+        $photo = $tour->file('image')->store('uploads', 'public');
+        Tour::create([
+            'tour' => request('tour'),
+            'content' => request('content'),
+            'price' => request('price'),
+            'photo' => $photo,
+        ]);
+        return back()->with('success','Дані були добавлені');
     }
 
     public function show($id)
     {
-      $tour = App\Tour::find($id);
-      return view('admin.tour.tour_show', compact('tour'));
+        $tour = App\Tour::find($id);
+        return view('admin.tour.tour_show', compact('tour'));
     }
 
     public function edit($id)
     {
-      $tour = App\Tour::find($id);
-      return view('admin.tour.edit_tour', compact('tour'));
+        $tour = App\Tour::find($id);
+        return view('admin.tour.edit_tour', compact('tour'));
     }
 
     public function delete($id)
     {
-      Tour::find($id)->delete();
-      return back();
+        Tour::find($id)->delete();
+        return back();
     }
 
     public function update_tur($id)
     {
-      Tour::find($id)->update([
-        'tour' => request('tour'),
-        'content' => request('content'),
-        'price' => request('price'),
-        'photo' => request('photo'),
-      ]);
-      return redirect('/admin/tour')->with('success','Дані були оновлені');
+        Tour::find($id)->update([
+            'tour' => request('tour'),
+            'content' => request('content'),
+            'price' => request('price'),
+            'photo' => request('photo'),
+        ]);
+        return redirect('/admin/tour')->with('success','Дані були оновлені');
     }
 }
