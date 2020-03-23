d3373a0c4c7411d1b861f33157747d7eb4618910
mishasvitaschyk <61791758+mishasvitaschyk@users.noreply.github.com>
2020-03-23 22:57:11 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,35 +12,35 @@
 */
 Route::group(['middleware' => 'auth', 'middleware' => 'access:admin'], function () {
 
-  Route::get('/admin', 'Admin\AdminMarkerController@index');
-  Route::get('admin/createmarker', 'Admin\AdminMarkerController@create');
-  Route::post('/markers_store', 'Admin\AdminMarkerController@store');
+    Route::get('/admin', 'Admin\AdminMarkerController@index');
+    Route::get('admin/createmarker', 'Admin\AdminMarkerController@create');
+    Route::post('/markers_store', 'Admin\AdminMarkerController@store');
 
-  //Route::post('/markers_store', 'MarkerController@store');
-  Route::get('/marker/{marker}','Admin\AdminMarkerController@show');
-  Route::get('/admin/marker/edit/{id}', 'Admin\AdminMarkerController@edit_marker')->name('edit-marker');
-  Route::post('/admin/marker/{id}/update','Admin\AdminMarkerController@update')->name('update-marker');
-  Route::get('/admin/marker/{id}/delete/','Admin\AdminMarkerController@delete')->name('delete');
+    //Route::post('/markers_store', 'MarkerController@store');
+    Route::get('/marker/{marker}','Admin\AdminMarkerController@show');
+    Route::get('/admin/marker/edit/{id}', 'Admin\AdminMarkerController@edit_marker')->name('edit-marker');
+    Route::post('/admin/marker/{id}/update','Admin\AdminMarkerController@update')->name('update-marker');
+    Route::get('/admin/marker/{id}/delete/','Admin\AdminMarkerController@delete')->name('delete');
 
 
-  Route::get('admin/tour', 'Admin\AdminTourController@index' );
-  Route::get('admin/createtour', 'Admin\AdminTourController@create');
-  Route::post('/create', 'Admin\AdminTourController@store');
-  Route::get('admin/tour/{tour}', 'Admin\AdminTourController@show')->name('admintourshow');
-  Route::get('admin/tour/{tour}/edit', 'Admin\AdminTourController@edit')->name('edit-tour');
-  Route::get('admin/tour/{tour}/delete', 'Admin\AdminTourController@delete')->name('delete_tour');
-  Route::post('admin/tour/{tour}/update', 'Admin\AdminTourController@update_tour')->name('update_tour');
+    Route::get('admin/tour', 'Admin\AdminTourController@index' );
+    Route::get('admin/createtour', 'Admin\AdminTourController@create');
+    Route::post('/create', 'Admin\AdminTourController@store');
+    Route::get('admin/tour/{tour}', 'Admin\AdminTourController@show')->name('admintourshow');
+    Route::get('admin/tour/{tour}/edit', 'Admin\AdminTourController@edit')->name('edit-tour');
+    Route::get('admin/tour/{tour}/delete', 'Admin\AdminTourController@delete')->name('delete_tour');
+    Route::post('admin/tour/{tour}/update', 'Admin\AdminTourController@update_tour')->name('update_tour');
     
-  Route::get('/maps/{id}','MarkerController@show')->name('show');
+    Route::get('/maps/{id}','MarkerController@show')->name('show');
     
-  Route::get('/tour/{id}','TourController@show')->name('tourshow');
-  Auth::routes();
+    Route::get('/tour/{id}','TourController@show')->name('tourshow');
+    Auth::routes();
   
-  Route::post('/comments/{id}','CommentController@marker_comment_store')->name('markercomment');
-  Route::post('/comments_tour/{id}', 'CommentController@tour_comment_store')->name('tour-comments');
+    Route::post('/comments/{id}','CommentController@marker_comment_store')->name('markercomment');
+    Route::post('/comments_tour/{id}', 'CommentController@tour_comment_store')->name('tour-comments');
 
-  Route::get('/tour/comment/delete/{id}', 'CommentController@comment_delete');
-  Route::get('/tour/comment/edit/{id}', 'CommentController@comment_edit');
+    Route::get('/tour/comment/delete/{id}', 'CommentController@comment_delete');
+    Route::get('/tour/comment/edit/{id}', 'CommentController@comment_edit');
     
 });
 Route::get('/','MarkerController@indexuser');
