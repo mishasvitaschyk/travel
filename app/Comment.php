9b1404f558663c522600c5ca217e2c3f0e4b71d7
mishasvitaschyk <61791758+mishasvitaschyk@users.noreply.github.com>
2020-03-23 22:45:09 +0200

    Update Comment.php

--- app/Comment.php
+++ app/Comment.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-  public function commentable()
-  {
-      return $this->morphTo();
-  }
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
   
-  protected $fillable = ['comment', 'user_id', 'user', 'commentable_id', 'commentable_type'];
+    protected $fillable = ['comment', 'user_id', 'user', 'commentable_id', 'commentable_type'];
 }
