71a277b65afd29dbbb033dc238f821ff3390950d
mishasvitaschyk <61791758+mishasvitaschyk@users.noreply.github.com>
2020-03-23 22:35:25 +0200

    Update tour.blade.php

--- resources/views/tour/tour.blade.php
+++ resources/views/tour/tour.blade.php
@@ -6,27 +6,20 @@
 @endsection
 
 @section('content')
-
 <div class="container">
-
-<br />
-
 <br />
 <br />
-
-
+<br />
 <div class="tur container jumbotron">
-  @foreach($tours as $tour)
+  @foreach ( $tours as $tour )
     <div class="tur-menu">
-
-      <a href="{{ route('tourshow',['tour' => $tour->id]) }}">
+      <a href="{{ route('tourshow', ['tour' => $tour->id]) }}">
       <img src="{{asset('/storage/'. $tour->photo)}}" width="100%" height="50%" alt="">
       </a>
       <h4 style="text-align:center;">{{$tour->tur}}</h4>
       <h6 style="text-align:justify;">{{$tour->content}}</h6>
       <h6> Ціна: {{$tour->price}} $</h6>
       <h6>Контакти: +(380)2856458</h6>
-
     </div>
   @endforeach
 </div>
